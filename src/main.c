dfa7ea68500591a631b8ea07d0ea5b228ee11be5
restitux <restitux@ohea.xyz>
2022-01-17 01:03:24 -0700

    Fixed player position moving between rooms
    and formatting

--- src/main.c
+++ src/main.c
@@ -8,8 +8,8 @@
 #include "../res/map/tiles.set.h"
 
 #include "player.h"
-#include "tilemap.h"
 #include "room_renderer.h"
+#include "tilemap.h"
 
 const unsigned int SCREEN_W = 160;
 const unsigned int SCREEN_H = 160;
@@ -18,53 +18,43 @@ const unsigned int SCREEN_H = 160;
 // const uint32_t tilesize = TILESIZE_tiles;
 
 struct PlayerData {
-  unsigned int x;
-  unsigned int y;
+    unsigned int x;
+    unsigned int y;
 };
 
-struct Player player = {
-  .loc = {
-    .room = {
-      0, 0
-    },
-    .screen = {
-      35, 70
-    }
-  }
-};
-struct Tilemap map = {testing_static_tilemap, testing_static_WIDTH, testing_static_HEIGHT, tiles_tileset};
-struct Datamap collision_map = {testing_collision_tilemap, testing_collision_WIDTH, testing_static_HEIGHT};
+struct Player player = {.loc = {.room = {0, 0}, .screen = {35, 70}}};
+struct Tilemap map = {testing_static_tilemap, testing_static_WIDTH,
+                      testing_static_HEIGHT, tiles_tileset};
+struct Datamap collision_map = {testing_collision_tilemap,
+                                testing_collision_WIDTH, testing_static_HEIGHT};
 
 void update() {
-  PALETTE[0] = 0x00FF0000;
-  PALETTE[1] = 0x00000000;
-  PALETTE[2] = 0x00444444;
-  PALETTE[3] = 0x00FFFFFF;
-  *DRAW_COLORS = 0x4321;
- 
-  
-  handle_movement(&player, &collision_map);
-  room_draw_room(player.loc.room.x, player.loc.room.y, &map);
+    PALETTE[0] = 0x00FF0000;
+    PALETTE[1] = 0x00000000;
+    PALETTE[2] = 0x00444444;
+    PALETTE[3] = 0x00FFFFFF;
+    *DRAW_COLORS = 0x4321;
 
-  *DRAW_COLORS = 0x4901;
-  draw_player(&player);
+    handle_movement(&player, &collision_map);
+    room_draw_room(player.loc.room.x, player.loc.room.y, &map);
 
+    *DRAW_COLORS = 0x4901;
+    draw_player(&player);
 
-  if (player.loc.screen.x >= 150) {
-      player.loc.screen.x = 80;
-      player.loc.room.x += 1;
-  }
-  if (player.loc.screen.x <= 10) {
-      player.loc.screen.x = 80;
-      player.loc.room.x -= 1;
-  }
-  if (player.loc.screen.y >= 150) {
-      player.loc.screen.y = 80;
-      player.loc.room.y += 1;
-  }
-  if (player.loc.screen.y <= 10) {
-      player.loc.screen.y = 80;
-      player.loc.room.y -= 1;
-
-  }
+    if (player.loc.screen.x >= 150) {
+        player.loc.screen.x = 11;
+        player.loc.room.x += 1;
+    }
+    if (player.loc.screen.x <= 10) {
+        player.loc.screen.x = 149;
+        player.loc.room.x -= 1;
+    }
+    if (player.loc.screen.y >= 150) {
+        player.loc.screen.y = 11;
+        player.loc.room.y += 1;
+    }
+    if (player.loc.screen.y <= 10) {
+        player.loc.screen.y = 149;
+        player.loc.room.y -= 1;
+    }
 }
