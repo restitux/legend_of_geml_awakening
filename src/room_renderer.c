b954669ef37aba52ef82bb1b7f4e8e3893bba6a0
restitux <restitux@ohea.xyz>
2022-01-17 00:19:50 -0700

    Fixed coords -> tile lookup and formatting

--- src/room_renderer.c
+++ src/room_renderer.c
@@ -2,13 +2,14 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#include "wasm4.h"
 #include "tilemap.h"
+#include "wasm4.h"
 
-void room_draw_tile(uint32_t tile, uint32_t x, uint32_t y, const uint8_t *tileset) {
-    uint32_t tile_id = (tile & 0xFF) - 1;
+void room_draw_tile(uint32_t tile, uint32_t x, uint32_t y,
+                    const uint8_t *tileset) {
+    uint32_t tile_id = (tile & 0xFF);
     uint32_t rotations = ((tile & (0xF << 28)) >> 28);
-    
+
     uint32_t flags = BLIT_2BPP;
 
     bool flipx = rotations & 0b1000;
@@ -31,23 +32,26 @@ void room_draw_tile(uint32_t tile, uint32_t x, uint32_t y, const uint8_t *tilese
 
     int byte = tile_id * 16;
 
-    blit(tileset + byte, x * 8, y * 8, 8, 8, flags);   
+    blit(tileset + byte, x * 8, y * 8, 8, 8, flags);
 }
 
-void room_draw_room(uint32_t room_x, uint32_t room_y, const struct Tilemap *map) {
+void room_draw_room(uint32_t room_x, uint32_t room_y,
+                    const struct Tilemap *map) {
     int room_start_y = room_y * 20;
     int room_start_x = room_x * 20;
     for (int x = room_start_x; x < room_start_x + 20; x++) {
         for (int y = room_start_y; y < room_start_y + 20; y++) {
             int tile_index = x + (y * map->map_width);
-            room_draw_tile(map->map[tile_index], x - room_start_x, y - room_start_y, map->tileset);
+            room_draw_tile(map->map[tile_index], x - room_start_x,
+                           y - room_start_y, map->tileset);
         }
     }
 }
 
-int room_tile_at_screen_coordinates(struct WorldCoordinate *loc, const struct Datamap *map) {
-    int world_x = (loc->screen.x / 8) * loc->room.x;
-    int world_y = (loc->screen.y / 8) * loc->room.y;
+int room_tile_at_screen_coordinates(struct WorldCoordinate *loc,
+                                    const struct Datamap *map) {
+    int world_x = (loc->screen.x / 8) + loc->room.x;
+    int world_y = (loc->screen.y / 8) + loc->room.y;
 
     int index = world_x + (world_y * map->map_width);
     return map->map[index];
