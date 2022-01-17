fd34fb473a0e7e5b819c81052c66b84a597e204a
restitux <restitux@ohea.xyz>
2022-01-17 00:20:18 -0700

    Fixed incorrect l/r dpad directions

--- src/player.c
+++ src/player.c
@@ -1,10 +1,10 @@
 #include "player.h"
-#include "wasm4.h"
 #include "room_renderer.h"
+#include "wasm4.h"
 
 void handle_movement(struct Player *player, struct Datamap *collision_map) {
     uint8_t gamepad = *GAMEPAD1;
-    
+
     if (gamepad & BUTTON_UP) {
         struct WorldCoordinate new_loc = player->loc;
         new_loc.screen.y -= 1;
@@ -23,7 +23,7 @@ void handle_movement(struct Player *player, struct Datamap *collision_map) {
     }
     if (gamepad & BUTTON_LEFT) {
         struct WorldCoordinate new_loc = player->loc;
-        new_loc.screen.x += 1;
+        new_loc.screen.x -= 1;
         int tile = room_tile_at_screen_coordinates(&new_loc, collision_map);
         if (!tile) {
             player->loc = new_loc;
@@ -31,7 +31,7 @@ void handle_movement(struct Player *player, struct Datamap *collision_map) {
     }
     if (gamepad & BUTTON_RIGHT) {
         struct WorldCoordinate new_loc = player->loc;
-        new_loc.screen.x -= 1;
+        new_loc.screen.x += 1;
         int tile = room_tile_at_screen_coordinates(&new_loc, collision_map);
         if (!tile) {
             player->loc = new_loc;
