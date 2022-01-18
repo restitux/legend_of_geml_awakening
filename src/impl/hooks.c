be5a1e3985a910fbb0b8903f1d247101e4a558f1
restitux <restitux@ohea.xyz>
2022-01-17 19:06:13 -0700

    Non working entrance interaction logic

--- src/impl/hooks.c
+++ src/impl/hooks.c
@@ -5,7 +5,7 @@
 
 #include "entrances.h"
 
-void on_room_enter() { populate_entrances(); }
+void on_room_enter() {}
 
 void on_room_exit() {}
 
@@ -24,4 +24,5 @@ void on_update() {
     draw_player(&game_state.player);
     room_draw_room(game_state.player.loc.room.x, game_state.player.loc.room.y,
                    &game_state.overworld->overlay_map);
+    // handle_entrances();
 }
